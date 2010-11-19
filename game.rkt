7a7396170ce06d7ed9e15a50d4f175f9e308dd02
Jason Baker <amnorvend@gmail.com>
2010-11-18 17:37:13 -0800

    Adding structs

--- game.rkt
+++ game.rkt
@@ -1,6 +1,14 @@
 #lang racket
 (require racket/sandbox)
-(provide play-game)
+(provide play-game board-spaces)
+
+(struct turn (player position))
+(struct player (name func))
+(struct state (unused moves-list user computer))
+
+(define board-spaces (for*/list ([row "abc"]
+                                [col '(1 2 3)])
+                               (format "~a~a" row col)))
 
 (define (code->func code)
   (lambda (moves-list)
@@ -10,13 +18,23 @@
       (kill-evaluator evaluator)
       result)))
       
-(define (take-turn moves-list func current-player other-player)
+(define (take-turn current-player current-state)
   (with-handlers ([exn:fail? (lambda (exn) other-player)])
-    (let ([result (func moves-list)])
-      (cons (list current-player result) moves-list))))
-      
+    (let ([result (func (state-moves-list current-state))])
+      (struct-copy state
+                   current-state
+                   [moves-list (cons (list current-player result) moves-list)])))
+
+(define (player-turn current-state)
+  
 
 (define (play-game my-code [moves-list '()])
-  (let ([my-func (code->func my-code)])
-    (take-turn moves-list my-func 'user 'computer)))
+  (let ([user (player 'x (code->func my-code))]
+        [computer (player 'o '())]
+        [initial-state (state board-spaces '() user computer)])
+    (take-turn initial-state)))
+  
+(define (turn->str turn-list)
+  (map
+   (lambda (turn) (format "~a placed at ~a" (turn-player turn) (turn-position turn)))))
   
